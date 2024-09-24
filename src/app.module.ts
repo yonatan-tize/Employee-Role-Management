cb18fb10bcfed91ef48b86d91c1c9038803f1b86
Yonatan Tizazu <yonatantizazu12@gmail.com>
2024-09-24 17:56:03 +0300

    created the role-management res

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RoleManagementModule } from './role-management/role-management.module';
 
 @Module({
-  imports: [],
+  imports: [RoleManagementModule],
   controllers: [AppController],
   providers: [AppService],
 })

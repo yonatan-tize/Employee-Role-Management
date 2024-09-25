93dee82f26bfbb1e8497f2ebfb5df217fb48b871
Yonatan Tizazu <yonatantizazu12@gmail.com>
2024-09-25 22:49:20 +0300

    performed Error handling

--- src/role-management/role-management.service.ts
+++ src/role-management/role-management.service.ts
@@ -1,9 +1,10 @@
-import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateRoleManagementDto } from './dto/create-role-management.dto';
 import { UpdateRoleManagementDto } from './dto/update-role-management.dto';
 import { Repository } from 'typeorm';
 import { RoleManagement } from './entities/role-management.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { error } from 'node:console';
 
 @Injectable()
 export class RoleManagementService {
@@ -14,120 +15,167 @@ export class RoleManagementService {
 
     // create a new role
     async create(createRoleManagementDto: CreateRoleManagementDto) {
-  
-      // Check if parent role exists (except for CEO which has no parent)
-      if (createRoleManagementDto.parentId !== null) {
-        const parentRole = await this.roleManagementRepository.findOne({
-          where: { id: createRoleManagementDto.parentId }
-        });
-        if (!parentRole) {
-          throw new NotFoundException({ message: "Parent Role Not Found" });
+      try{
+        // Check if parent role exists (except for CEO which has no parent)
+        if (createRoleManagementDto.parentId !== null) {
+          const parentRole = await this.roleManagementRepository.findOne({
+            where: { id: createRoleManagementDto.parentId }
+          });
+          if (!parentRole) {
+            throw new NotFoundException({ message: "Parent Role Not Found" });
+          }
         }
+      
+        // Check if the role already exists
+        const existingRole = await this.roleManagementRepository.findOne({
+          where: { name: createRoleManagementDto.name }
+        });
+        if (existingRole) {
+          throw new ConflictException({ message: "Role Already Exists" });
+        }
+      
+        // Check if the CEO role is trying to have a parent
+        if (createRoleManagementDto.name === "CEO" && createRoleManagementDto.parentId !== null) {
+          throw new BadRequestException({ message: "CEO Cannot Have A Parent" });
+        }
+      
+        // Create and save the new role
+        const newRole = this.roleManagementRepository.create(createRoleManagementDto);
+        return await this.roleManagementRepository.save(newRole);
+      } catch(err){
+          throw new InternalServerErrorException(
+            {
+              message: "Failed to Create Role",
+              error: err.message
+            }
+          )
       }
-    
-      // Check if the role already exists
-      const existingRole = await this.roleManagementRepository.findOne({
-        where: { name: createRoleManagementDto.name }
-      });
-      if (existingRole) {
-        throw new ConflictException({ message: "Role Already Exists" });
-      }
-    
-      // Check if the CEO role is trying to have a parent
-      if (createRoleManagementDto.name === "CEO" && createRoleManagementDto.parentId !== null) {
-        throw new BadRequestException({ message: "CEO Cannot Have A Parent" });
-      }
-    
-      // Create and save the new role
-      const newRole = this.roleManagementRepository.create(createRoleManagementDto);
-      return await this.roleManagementRepository.save(newRole);
     }
     
 
     //find all roles
     async findAll() {
-      return await this.roleManagementRepository.find()
+      try{
+        return await this.roleManagementRepository.find()
+      } catch(err){
+        throw new InternalServerErrorException(
+          {
+            message: "Failed to Get Roles",
+            error: err.message
+          }
+        )
+      }
     }
 
 
     // find role by id
     async findOne(id: number) {
+      try{
+        //check if role already exists
+        const role = await this.roleManagementRepository.findOne({
+          where: { id }
+        });
 
-      //check if role already exists
-      const role = await this.roleManagementRepository.findOne({
-        where: { id }
-      });
+        if (!role) throw new NotFoundException({message: "Role Not Found"});
 
-      if (!role) throw new NotFoundException({message: "role not found"});
-
-      return role;
+        return role;
+      }catch(err){
+        throw new InternalServerErrorException(
+          {
+            message: "Failed to Get Role",
+            error: err.message
+          }
+        )
+      }
     }
 
 
     // Get all childrens of a specific position/role
     async findChildren(id: number){
+      try{
+        //check if role already exists
+        const role = await this.roleManagementRepository.findOne({ where: { id } });
+        if(!role) throw new NotFoundException({message: "Role Not Found"});
 
-      //check if role already exists
-      const role = await this.roleManagementRepository.findOne({ where: { id } });
-      if(!role) throw new NotFoundException({message: "Role Not Found"});
+        // check if role has children
+        const children = await this.roleManagementRepository.find({
+          where: { parentId: id }
+        }); 
+        if (!children) throw new NotFoundException({message: `No Child For ${role.name} `}) 
 
-      // check if role has children
-      const children = await this.roleManagementRepository.find({
-        where: { parentId: id }
-      }); 
-      if (!children) throw new NotFoundException({message: `No Child For ${role.name} `}) 
-
-      return children
+        return children
+      } catch(err){
+          throw new InternalServerErrorException(
+            {
+              message: "Failed to Get Children",
+              error: err.message
+            }
+          )
+      }
     }
       
     
     // Updates a role for the given id.
     async update(id: number, updateRoleManagementDto: UpdateRoleManagementDto) {
+      try{
+        //check if role already exists
+        const role = await this.roleManagementRepository.findOne({
+          where: { id }
+        });
+        if (!role) throw new NotFoundException({message: "Role Not Found"});
 
-      //check if role already exists
-      const role = await this.roleManagementRepository.findOne({
-        where: { id }
-      });
-      if (!role) throw new NotFoundException({message: "Role Not Found"});
+        //check if the role is CEO and is trying to get parent
+        if (role.name === "CEO" && updateRoleManagementDto.parentId !== null ){
+          throw new BadRequestException({message: "CEO Cannot Have A Parent"})
+        }
 
-      //check if the role is ceo and is trying to get parent
-      if (role.name === "CEO" && updateRoleManagementDto.parentId !== null ){
-        throw new BadRequestException({message: "CEO Cannot Have A Parent"})
+        // check self reference
+        if (updateRoleManagementDto.parentId === role.id){
+          throw new BadRequestException({message: 'Can not Reference Self As Parent'})
+        }
+
+        // check if the parent role already exists
+        if (updateRoleManagementDto.parentId){
+          const parentRole = await this.roleManagementRepository.find({
+            where: { parentId : updateRoleManagementDto.parentId }
+          })
+          if (!parentRole) throw new NotFoundException({message: "Parent Not Found"})
+        }
+
+        return await this.roleManagementRepository.update(id, updateRoleManagementDto);
+      } catch(err){
+          throw new InternalServerErrorException(
+            {
+              message: "Failed to Update Role",
+              error: err.message
+            }
+          )
       }
-
-      // check self reference
-      if (updateRoleManagementDto.parentId === role.id){
-        throw new BadRequestException({message: 'Can not Reference Self As Parent'})
-      }
-
-      // check if the parent role already exists
-      if (updateRoleManagementDto.parentId){
-        const parentRole = await this.roleManagementRepository.find({
-          where: { parentId : updateRoleManagementDto.parentId }
-        })
-        if (!parentRole) throw new NotFoundException({message: "Parent Not Found"})
-      }
-
-      return await this.roleManagementRepository.update(id, updateRoleManagementDto);
     }
 
 
     // find the role by id and if exist delete the role
     async remove(id: number) {
+      try{
+        //check if role already exists
+        const role = await this.roleManagementRepository.findOne({
+          where: { id }
+        });
+        if (!role) throw new NotFoundException({message: "Role Not Found"});
 
-      //check if role already exists
-      const role = await this.roleManagementRepository.findOne({
-        where: { id }
-      });
-      if (!role) throw new NotFoundException({message: "Role Not Found"});
+        // If the role has children, update their parentId to the parentId of the role being deleted
+        await this.roleManagementRepository.update(
+          {parentId: id},  
+          {parentId: role.parentId}
+        )
 
-      // If the role has children, update their parentId to the parentId of the role being deleted
-      await this.roleManagementRepository.update(
-        {parentId: id},  
-        {parentId: role.parentId}
-      )
-
-      //delete the row
-      return await this.roleManagementRepository.delete(id);
+        //delete the row
+        return await this.roleManagementRepository.delete(id);
+      } catch(err){
+          throw new InternalServerErrorException({
+            message: 'Failed to Remove Role',
+            error: err.message
+          });
+      }
     }
 }
